eb815e1724f2d05f21e5e18374f295fa9d3241a9
gone-still <eldesgraciado@gmail.com>
2024-01-09 18:31:28 -0600

    Updated VendingMachine

--- 03 - vendingMachine/VendingMachine.py
+++ 03 - vendingMachine/VendingMachine.py
@@ -1,7 +1,7 @@
 # File        :   VendingMachine.py
-# Version     :   0.5.0
+# Version     :   1.0.0
 # Description :   Vending Machine implementation
-# Date:       :   Jan 08, 2023
+# Date:       :   Jan 09, 2023
 # Author      :   Ricardo Acevedo-Avila (racevedoaa@gmail.com)
 # License     :   Creative Commons CC0
 
@@ -11,33 +11,32 @@ import pprint
 class VendingMachine:
     # Class variable, shared by all instances:
     machineType = "Vending Machine"
+    machineVersion = "1.0"
 
     # Initialization:
-    def __init__(self, name="Default", products=None) -> None:
+    def __init__(self, name: str = "Default", products: list = None) -> None:
 
         print("Initializing ", self.machineType)
 
         # Machine name:
-        self.name = name
+        self._name = name
         # List of available products:
-        self.stock = {}
+        self._stock = {}
         # List of option -> product:
-        self.productOptions = []
-        # Money entered:
-        self.inputMoney = 0
+        self._productOptions = []
         # Current Selection:
-        self.currentProduct = -1
-        # Change:
-        self.currentChange = 0
+        self._currentProduct = -1
         # Total items (units/products):
-        self.totalItems = 0
-
+        self._totalItems = 0
         # Empty flag:
-        self.empty = False
+        self._empty = False
+
+        # Verbosity output:
+        self._verbose = False
 
         # Fill with default products:
         if not products:
-            print("Init>> No products given, using default.")
+            self.display("No products given, using default.")
 
             # Create default products:
             initProducts = [{"Product ID": "Soda-1", "Price": 2.5, "Units": 1},
@@ -47,23 +46,29 @@ class VendingMachine:
             # Fill machine with default products:
             self.fillMachine(initProducts)
 
-        # Execute vending machine FSM:
-        self.machineFSM()
-
     # Total products:
     def __len__(self):
         # Return the number of items currently in stock:
         totalItems = 0
-        for k, v in self.stock.items():
-            # get current units:
+        for k, v in self._stock.items():
+            # Get current units:
             productUnits = v["Units"]
             totalItems += productUnits
 
         return totalItems
 
+    # Set verbose:
+    def setVerbose(self, verbose: bool) -> None:
+        self._verbose = verbose
+        print("Verbose set to: ", self._verbose)
+
+    # Print to machine's display:
+    def display(self, message: str) -> None:
+        print(self._name + ": " + message)
+
     # Setup stock of products:
-    def fillMachine(self, products) -> None:
-        print("Filling machine stock with products...")
+    def fillMachine(self, products: list) -> None:
+        self.display("Filling machine stock with products...")
 
         # products is the list of products, which are dictionaries entries
         # containing the key -> Product ID, the price, and the product quantities
@@ -78,98 +83,121 @@ class VendingMachine:
 
                 # Add to stock "entry: (key - default initialization)
                 productPrice = currentProduct["Price"]
-                self.stock.setdefault(productType, {"Price": productPrice, "Units": 0})
+                self._stock.setdefault(productType, {"Price": productPrice, "Units": 0})
                 # Add units to "entry":
-                self.stock[productType]["Units"] += productUnits
+                self._stock[productType]["Units"] += productUnits
 
-                print("Product Added:", productType, "Price: ", productPrice, "Units:", productUnits)
+                if self._verbose:
+                    print("Product Added:", productType, "Price: ", productPrice, "Units:", productUnits)
 
             else:
-                print("Product:", productType, " got no units, not added to stock.")
+                if self._verbose:
+                    print("Product:", productType, " got no units, not added to stock.")
 
-        print("Current Stock:")
+        self.display("Current Stock:")
         pp = pprint.PrettyPrinter(width=1)
-        pp.pprint(self.stock)
+        pp.pprint(self._stock)
 
         # Create options from stock:
         self.createOptions()
 
         # Get total products:
-        self.totalItems = len(self)
-        print("Total items available: ", self.totalItems)
-
-        # Is the machine empty?
-        if self.totalItems < 1:
-            self.empty = True
+        self._totalItems = len(self)
+        self.display("Total items available: " + str(self._totalItems))
 
     # Create options menu from stock:
     def createOptions(self) -> None:
 
-        print("Creating options from stock products...")
+        self.display("Creating options from stock products...")
 
         # Loop through the stock and assign each product an
         # option to be displayed in the menu:
-        for i, k in enumerate(self.stock):
-            self.productOptions.append(k)
+        for i, k in enumerate(self._stock):
+            self._productOptions.append(k)
 
     # Updates the stock:
-    def updateStock(self, item) -> None:
+    def updateStock(self, item: int) -> None:
 
-        print("Updating stock...")
+        self.display("Updating stock...")
 
         # Get product name:
-        productName = self.productOptions[item]
+        productName = self._productOptions[item]
         # Get original quantity:
-        pastUnits = self.stock[productName]["Units"]
+        pastUnits = self._stock[productName]["Units"]
         # Subtract item:
-        self.stock[productName]["Units"] -= 1
-        currentUnits = self.stock[productName]["Units"]
+        self._stock[productName]["Units"] -= 1
+        currentUnits = self._stock[productName]["Units"]
 
-        print("Updated item: " + productName + " : " + str(pastUnits) + " -> " + str(currentUnits))
+        self.display("Updated item: " + productName + " : " + str(pastUnits) + " -> " + str(currentUnits))
+
+        # Get total products:
+        self._totalItems = len(self)
+
+        # Is the machine empty?
+        if self._verbose:
+            print("Total items: ", self._totalItems)
+        if self._totalItems < 1:
+            # Machine is empty:
+            self._empty = True
 
     # Display menu:
-    def displayMenu(self) -> None:
+    def displayMenu(self, extraOptions: list = None) -> None:
+
         print("* * ======{ Menu }====== * *")
 
         # Get total items:
-        print(self.machineType, "currently has: ", self.totalItems, "item(s).")
+        print(self.machineType, "currently has: ", self._totalItems, "item(s).")
 
         print("> Select an option from the available products: ")
 
         # Display options of available product:
-        totalOptions = len(self.productOptions)
+        totalOptions = len(self._productOptions)
         for i in range(totalOptions):
             # Get product name:
-            productName = self.productOptions[i]
+            productName = self._productOptions[i]
             # Price:
-            productPrice = self.stock[productName]["Price"]
+            productPrice = self._stock[productName]["Price"]
             # Quantity:
-            productQuantity = self.stock[productName]["Units"]
+            productQuantity = self._stock[productName]["Units"]
 
             # Build option string:
             optionString = " I: " + productName + "   P: " + str(productPrice) + " $   Q: " + str(productQuantity)
-            # Show current option
+            # Show current option:
             print("[" + str(i) + "]", " ---> ", optionString)
 
-    # Processes item selection:
-    def processSelection(self, currentSelection) -> bool:
+        # Display extra options:
+        if extraOptions:
+            for currentTuple in extraOptions:
+                # Get symbol
+                extraOption = currentTuple[0]
+                # Get description:
+                optionInfo = currentTuple[1]
 
-        self.currentProduct = currentSelection
-        print("Your selection:", self.currentProduct)
+                # Show current option:
+                print("[" + str(extraOption) + "]", " ---> ", optionInfo + "  ", end=" ")
+
+        # Print new line:
+        print("\n")
+
+    # Processes item selection:
+    def processSelection(self, item: int) -> bool:
+
+        self._currentProduct = item
+        self.display("Your selection: " + str(self._currentProduct))
 
         # Show product name, price and quantity:
-        productName = self.productOptions[self.currentProduct]
-        productPrice = self.stock[productName]["Price"]
-        productQuantity = self.stock[productName]["Units"]
+        productName = self._productOptions[self._currentProduct]
+        productPrice = self._stock[productName]["Price"]
+        productQuantity = self._stock[productName]["Units"]
 
-        print("Selected item: " + productName + " (x" + str(productQuantity) + ")")
-        print("Price: " + str(productPrice))
+        self.display("Selected item: " + productName + " (x" + str(productQuantity) + ")")
+        self.display("Price: " + str(productPrice))
 
         # Default flag value (no product is given):
         giveItem = False
 
         # Check at least one quantity of the item is available:
-        if productQuantity > 1:
+        if productQuantity >= 1:
 
             # Receive money from user:
             userMoney = float(input("Enter money: "))
@@ -179,63 +207,47 @@ class VendingMachine:
 
             # If difference >= epsilon, got enough money
             if moneyDifference >= 0:
-                print("You entered: " + str(userMoney) + " $")
+                if self._verbose:
+                    print("You entered: " + str(userMoney) + " $")
                 # Compute change:
                 moneyChange = moneyDifference
                 giveItem = True
 
             # Else user is broke:
             else:
-                print("You are broke. Come back when you have enough money.")
+                self.display("You are broke. Come back when you have enough money.")
                 moneyChange = userMoney
 
             # Give change:
-            print("> Your change: " + str(moneyChange) + " $")
+            self.display("> Your change: " + str(moneyChange) + " $")
 
         # No quantities left from this item:
         else:
-            print("No units left for this item. Please select another one.")
+            self.display("No units left for this item. Please select another one.")
 
         return giveItem
 
     # Serves the item:
-    def serveProduct(self, item):
-        productName = self.productOptions[item]
-        print("> Here's your product: ", productName)
+    def serveProduct(self, item: int):
+        productName = self._productOptions[item]
+        self.display("> Here's your product: " + productName)
 
-    # Default greeting:
-    def displayGreeting(self) -> None:
-        print("Hello from: ", self.name, "!")
-        print("Type: ", self.machineType)
+    # Returns the list of options:
+    @property
+    def productOptions(self) -> list:
+        # Read only attribute:
+        return self._productOptions
 
-    # Executes the vending machine FSM:
-    def machineFSM(self) -> None:
+    # Shuts down machine:
+    def shutdownMachine(self) -> None:
+        self.display("Bye!")
 
-        # Greet:
-        self.displayGreeting()
-
-        # Display menu:
-        self.displayMenu()
-
-        # Check if the machine is empty
-        if not self.empty:
-
-            # Receive input from user:
-            currentSelection = input("Enter item: ")
-            currentSelection = int(currentSelection)
-
-            giveItem = self.processSelection(currentSelection)
-
-            # Has the product been dispatched?
-            if giveItem:
-                # Serve product:
-                self.serveProduct(currentSelection)
-
-                # Update stock:
-                self.updateStock(currentSelection)
-
-            # Ready for next costumer:
-            print("Come back anytime!")
-
-        else:
-            print("Out of product. Come back later!")
+    # Displays machine info:
+    def getMachineInfo(self) -> None:
+        print("==== Machine Info: ==== ")
+        print("Type: " + str(self.machineType))
+        print("Name: " + str(self._name))
+        print("Ver: " + str(self.machineVersion))
+        print("Current Stock:")
+        pp = pprint.PrettyPrinter(width=1)
+        pp.pprint(self._stock)
--- 03 - vendingMachine/main.py
+++ 03 - vendingMachine/main.py
@@ -1,11 +1,112 @@
 # File        :   main.py
-# Version     :   0.5.0
+# Version     :   1.0.0
 # Description :   Vending Machine instantiation
-# Date:       :   Jan 08, 2023
+# Date:       :   Jan 09, 2023
 # Author      :   Ricardo Acevedo-Avila (racevedoaa@gmail.com)
 # License     :   Creative Commons CC0
 
 from VendingMachine import VendingMachine
 
+
+def getMenuOptions(machine: VendingMachine, extraOptions: list) -> dict:
+    """
+    Receives a VendingMachine instance and a list of extra
+    options and returns a dict of 2 lists: one list with numerical
+    products indices and another list with character options.
+    Both lists store strings
+    """
+    # Create list of extra options:
+    menuList = [options[0] for options in extraOptions]
+
+    # Create list of options:
+    productList = list(range(0, len(machine.productOptions)))
+    # Int to string
+    productList = [str(number) for number in productList]
+
+    return {"Products": productList, "Menu": menuList}
+
+
 # Create a new vending machine:
 newMachine = VendingMachine(name="Dracula")
+
+# Set verbosity:
+newMachine.setVerbose(False)
+
+# Set Additional options to be displayed in menu:
+extraOptions = [("E", "Exit"), ("I", "Info")]
+
+# Get lists of available menu options:
+optionsDict = getMenuOptions(newMachine, extraOptions)
+productOptions = optionsDict["Products"]
+menuOptions = optionsDict["Menu"]
+
+# Loop control:
+runMachine = True
+
+# Greet:
+greetMessage = "Hello from " + newMachine.machineType + ". My name is: " + newMachine._name
+newMachine.display(greetMessage)
+
+while runMachine:
+
+    # Display menu:
+    newMachine.displayMenu(extraOptions)
+
+    # Check if the machine is empty
+    machineEmpty = newMachine._empty
+
+    # If no empty, run the loop:
+    if not machineEmpty:
+
+        # Receive input from user:
+        currentOption = input("Enter selection: ")
+
+        # Check if numerical option:
+        if currentOption in productOptions:
+
+            # Convert to int:
+            currentOption = int(currentOption)
+
+            # Process product selection:
+            giveItem = newMachine.processSelection(currentOption)
+
+            # Has the product been dispatched?
+            if giveItem:
+                # Serve product:
+                newMachine.serveProduct(currentOption)
+
+                # Update stock:
+                newMachine.updateStock(currentOption)
+
+            # Ready for next costumer:
+            newMachine.display("Have a nice day!")
+
+        else:
+
+            # To uppercase:
+            currentOption = currentOption.upper()
+
+            # Check if extra option:
+            if currentOption in menuOptions:
+
+                # Exit?
+                if currentOption.upper() == "E":
+                    # Say goodbye and exit:
+                    newMachine.shutdownMachine()
+                    runMachine = False
+
+                else:
+
+                    # Display info?
+                    if currentOption.upper() == "I":
+                        newMachine.getMachineInfo()
+                    else:
+                        newMachine.display("Invalid option, try again.")
+
+            else:
+                newMachine.display("Invalid option, try again.")
+
+    else:
+        # There are no items left:
+        newMachine.display("Out of product. Come back later!")
+        runMachine = False
